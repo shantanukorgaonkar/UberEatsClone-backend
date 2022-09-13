589505f06f25feb33a2801ee6255b856ac85c69a
Shantanu Korgaonkar <shantanukorgaonkar7@gmail.com>
2022-09-13 13:04:42 +0530

    ADD Util methods

---
+++ Utils/Utils.js
@@ -0,0 +1,25 @@
+const sendSuccess = (res, statusCode, message, data) => {
+    const status = 'Success'
+    const result = {
+        status,
+        message,
+        data
+    }
+    return res.status(statusCode).json(result)
+
+}
+
+const sendError=(res,statusCode,message)=>{
+
+    const status = 'Error'
+    const result = {
+        status,
+        message
+    }
+    return res.status(statusCode).json(result)
+}
+
+module.exports = {
+    sendSuccess,
+    sendError
+}
