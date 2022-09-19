1e78ff5d6b0bc3990e355078c1de1fd5c24a773d
Shantanu Korgaonkar <shantanukorgaonkar7@gmail.com>
2022-09-19 20:22:59 +0530

    ADD validation

---
+++ Utils/validation.js
@@ -0,0 +1,13 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const hashPassword =async (password)=>{
+
+const salt = await bcrypt.genSalt(10);
+
+const hashedPassword = await bcrypt.hash(password,salt);
+
+return hashedPassword
+}
+
+module.exports = {hashPassword}
