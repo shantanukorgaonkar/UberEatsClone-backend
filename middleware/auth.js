2dfca4001e7c9d57e9740b642aa8453cdbf6ebc3
Shantanu Korgaonkar <shantanukorgaonkar7@gmail.com>
2022-09-19 23:10:49 +0530

    ADD auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,38 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+const { sendError } = require('../Utils/Utils');
+
+
+const VerifyAuth = async (req, res, next) => {
+    const incomingAuthToken = req.headers.authorization;
+
+
+    if (!incomingAuthToken) {
+        return sendError(res, 401, "Not Authorised");
+    }
+
+    if (incomingAuthToken.startsWith('Bearer')) {
+        try {
+            const token = incomingAuthToken.split(' ')[1];
+
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            req.user = await User.findById(decoded.id).select('-password');
+
+            next();
+
+        } catch (error) {
+
+            console.log(error)
+            return sendError(res, 400, "Invalid Token")
+
+        }
+
+    } else {
+        return sendError(res, 400, "Invalid Token")
+    }
+
+
+}
+
+module.exports = { VerifyAuth } 
